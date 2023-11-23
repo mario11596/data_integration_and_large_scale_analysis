679375899ef8ee0c58107639ca7771cac2bc5092
mario_negovetic <mario.negovetic@estudent.hr>
2023-11-23 23:09:11 +0100

    changed feature encoding

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import configparser
 import pandas as pd
-from category_encoders import *
+from sklearn.preprocessing import OneHotEncoder
 
 
 config = configparser.ConfigParser()
@@ -9,25 +9,26 @@ config = config['default']
 filter_file = config['filepath_raw']
 def filter_columns_file():
     data_file = pd.read_csv(filepath_or_buffer=filter_file, delimiter=',', low_memory=False)
-    specific_column = data_file["NAME"]
 
-    # string only has numbers
-    check_contain_numeric_name = specific_column.str.isnumeric()
+    # delete records with only numbers in name
+    check_contain_numeric_name = data_file["NAME"].str.isnumeric()
     index = check_contain_numeric_name.index[check_contain_numeric_name == True].tolist()
     data_file.drop(labels=index, axis=0, inplace=True)
 
-    #string has strange letters
+    # remove symbols from name
     data_file["NAME"].replace('\W', '', regex=True, inplace=True)
-    data_file.to_csv(filter_file, index=False, sep=',')
+    data_file["NAME"].replace('', None, regex=True, inplace=True)
 
-    #strring has null
+    # delete records that have null name
     data_file.dropna(subset=['NAME'], inplace=True)
     data_file.to_csv(filter_file, index=False, sep=',')
 
 def state_feature_encoding():
     data_file = pd.read_csv(filepath_or_buffer=filter_file, delimiter=',', low_memory=False)
-    state_counts = data_file['STATE'].value_counts().to_dict()
-    data_file['STATE_ENCODED'] = data_file['STATE'].map(state_counts)
+
+    encoder = OneHotEncoder()
+    columnes = ['state_0', 'state_1', 'state_2','state_3','state_4','state_5','state_6','state_7','state_8']
+    data_file[columnes] = pd.DataFrame(encoder.fit_transform(data_file[['STATE']]).toarray())
     data_file.to_csv(filter_file, index=False, sep=',')
 
 
@@ -36,9 +37,8 @@ def city_feature_encoding():
 
     encoder = TargetEncoder()
     encoder.fit(X=data_file['CITY'], y=data_file['PHONEAREACODE'])
-
-    values = encoder.transform(data_file['CITY_ENCODED'])
-    data = pd.concat([data_file, values], axis=1)
+    data_file['CITY_ENCODING'] = encoder.transform(data_file['CITY'])
+    data_file.to_csv(filter_file, index=False, sep=',')
 
 
 if __name__ == '__main__':
